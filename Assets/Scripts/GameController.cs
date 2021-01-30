031091fcb11315b5e354c6fd76c7918f267c3cfe
MisaJovanovic <misaj@nordeus.com>
2021-01-30 17:24:20 +0100

    ships and stuff

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+	public CameraController Camera;
+
+	void Start()
+    {
+
+    }
+
+    void Update()
+    {
+		Camera.OnUpdate();
+	}
+}
