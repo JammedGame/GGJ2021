85466b66252243707796e0ce5fa75c0252bb76c1
Djordje Cvijic <djordjec@nordeus.com>
2021-01-31 20:30:32 +0100

    dialog

---
+++ Assets/Scripts/Settings/DialogSettings.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Settings
+{
+	[CreateAssetMenu]
+	public class DialogSettings : ScriptableObject
+	{
+		public List<Dialog> dialogs;
+	}
+
+	[Serializable]
+	public class Dialog
+	{
+		public bool isEpilogue;
+		public PirateSettings triggerWhenFound;
+		public List<DialogLine> lines;
+	}
+
+	[Serializable]
+	public class DialogLine
+	{
+		public PirateSettings pirate;
+		public string text;
+		public DialogCondition condition;
+	}
+
+	[Serializable]
+	public class DialogCondition
+	{
+		public PirateSettings pirate;
+		public DialogConditionOperator op;
+	}
+
+	public enum DialogConditionOperator
+	{
+		NotFound,
+		Found
+	}
+}
---
+++ Assets/Scripts/Settings/PirateSettings.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Settings
+{
+	[CreateAssetMenu]
+	public class PirateSettings : ScriptableObject
+	{
+		public string pirateName;
+		public string title;
+		public GameObject prefab;
+	}
+}
