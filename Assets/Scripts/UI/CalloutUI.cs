7bc0d1cc96d2c1ef2fc666c1ce538b44a82a02fa
Djordje Cvijic <djordjec@nordeus.com>
2021-01-31 14:45:01 +0100

    UI stuff

---
+++ Assets/Scripts/UI/CalloutUI.cs
@@ -0,0 +1,10 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+	public class CalloutUI : MonoBehaviour
+	{
+		public TextMeshProUGUI titleLabel;
+	}
+}
---
+++ Assets/Scripts/UI/DialogUI.cs
@@ -0,0 +1,13 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+	public class DialogUI : MonoBehaviour
+	{
+		public RectTransform avatar;
+		public TextMeshProUGUI titleLabel;
+		public TextMeshProUGUI textLabel;
+		public RectTransform footer;
+	}
+}
